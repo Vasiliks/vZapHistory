b80959c0873fdbc03b65f31dae4a9dd368628ae9
Vasiliks <vasiliks@narod.ru>
2022-03-27 12:54:14 +0200

    Add python3 support

--- setup.py
+++ setup.py
@@ -1,15 +1,30 @@
+from __future__ import print_function
 from distutils.core import setup
-import setup_translate
+import os
 
+PLUGIN_DIR = 'Extensions.vZapHistory'
+
+def compile_translate():
+	for lang in os.listdir('po'):
+		if lang.endswith('.po'):
+			src = os.path.join('po', lang)
+			lang = lang[:-3]
+			destdir = os.path.join('src/locale', lang, 'LC_MESSAGES')
+			if not os.path.exists(destdir):
+				os.makedirs(destdir)
+			dest = os.path.join(destdir, 'vZapHistory.mo')
+			print("Language compile %s -> %s" % (src, dest))
+			if os.system("msgfmt '%s' -o '%s'" % (src, dest)) != 0:
+				raise RuntimeError("Failed to compile", src)
+
+compile_translate()
 
 setup(name = 'enigma2-plugin-extensions-vzaphistory',
 		version='1.0',
 		author='Vasiliks',
 		author_email='vasiliks73@gmail.com',
-		package_dir = {'Extensions.vZapHistory': 'src'},
-		packages=['Extensions.vZapHistory'],
-		package_data={'Extensions.vZapHistory': ['buttons/*.png']},
+		package_dir = {PLUGIN_DIR: 'src'},
+		packages=[PLUGIN_DIR],
+		package_data={PLUGIN_DIR: ['buttons/*.png']},
 		description = 'Quick zapping between last viewed channels',
-		cmdclass = setup_translate.cmdclass,
 	)
-
